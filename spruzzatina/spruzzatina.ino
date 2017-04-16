275244b5fe12a4a33a7122336cc67c79ca12a33d
bsn-iam <bsn-iam@yandex.ru>
2017-04-16 23:11:55 +0300

    First test on hardware

--- spruzzatina/spruzzatina.ino
+++ spruzzatina/spruzzatina.ino
@@ -1,18 +1,19 @@
 
 
 const int feederAmount=5;
-const int pinValves[feederAmount] = {1, 2, 3, 4, 5}; //digital out
+const int pinValves[feederAmount] = {2, 3, 5, 7, 9}; //digital out
 const int pinOrders[feederAmount]= {5, 6, 7, 8, 9}; //analog in 
 
 
-const int pinManualRun = 15;  //digital in
-const int pinConsumption = 15;  //digital in
-const int pinPump = 15;   //digital out
-const int pinBuzzer = 15;   //digital out
+const int pinManualRun = 2;  //digital in
+const int pinConsumptionPower = 11;  //digital OUT
+const int pinConsumption = 12;  //digital in
+const int pinPump = 13;   //digital out
+const int pinBuzzer = 11;   //digital out
 
 
-int volumes[5];
-float deadOrder=0.15; //0-5v
+float volumes[5];
+float deadVolumeOrder=20; //mL
 const int SerialBaud = 19200; //UART port speed
 
 unsigned long const stepTime=1000*60*60*24;
@@ -25,7 +26,7 @@ int flowAbsentDelay=2*1000;
 
 float currentFlow;
 float currentVolume;
-int maximumVolume=200; //mL
+float maximumVolume=200; //mL
 float pumpTableFlow=0.03; //mL/ms
 float minPossibleFlow=0.001; //mL/ms
 //int maximumDuration=30000; //ms till the stop
@@ -37,8 +38,12 @@ void initPins() {
 
 	pinMode(pinManualRun, INPUT);
 	digitalWrite(pinManualRun, LOW);
+	
+	pinMode(pinConsumptionPower, INPUT);
+	digitalWrite(pinConsumptionPower, LOW);
 
 	pinMode(pinConsumption, INPUT);
+	digitalWrite(pinConsumption, LOW);
   
 	for (int index = 0; index <= feederAmount-1; index++) {
 		pinMode(pinValves[index], OUTPUT);
@@ -49,16 +54,23 @@ return;
 
 void setup() {
 	initPins();
+	startTheSensor();
 	getStartSound();
+	volumes[0]=200;
+	volumes[1]=200;
+	volumes[2]=0;
+	volumes[3]=0;
+	volumes[4]=0;
 }
 
 void loop() {
-	while (millis()-lastFinishTime < stepTime){
+	while (millis()-lastFinishTime > stepTime){
 		checkManualRun();
 		delay(1000);
 	}
 	runFullCycle();
 	lastFinishTime=millis();
+	delay(5000);
 }
 
 void runFullCycle(){
@@ -76,6 +88,12 @@ void checkManualRun(){
 return;	
 }
 
+float getOrder(int index){
+	float rawVolume=analogRead(pinOrders[index]); //0-5v
+	volumes[index]=rawVolume/5.0*maximumVolume;
+return volumes[index];	
+}
+
 float GetFlow() {
 	uint32_t varPulse; 
 	float flow;
@@ -84,20 +102,19 @@ float GetFlow() {
 	if(varPulse){flow=1000000/(15*varPulse);}
     else        {flow=0;}  //L/min  
 	flow=flow*1000/60*1000; //mL/ms
-	Serial.println((String) "Current flow [" + flow + "] mL/ms].");
+	//Serial.println((String) "Current flow [" + flow + "] mL/ms].Pulse duration-"+varPulse);
 return flow; //mL/ms
 }
 
 
 void FeedTheLine(int index){
 	int counter=0;
-	int rawVolume=analogRead(pinOrders[index]); //0-5v
-	//volumes[index]=rawVolume; //0-5
-	float requestedVolume=rawVolume/5*maximumVolume; //mL
+	//float requestedVolume=getOrder(index); //mL
+	float requestedVolume=volumes[index]; //mL
 	int requestedDuration=requestedVolume/pumpTableFlow; //ms
-	Serial.println((String) "Feeder [" + index + "] has order ["+volumes[index]+"].");
 
-	if (rawVolume > deadOrder){
+	if (requestedVolume > deadVolumeOrder){
+		Serial.println((String) "Feeder [" + index + "] has order ["+requestedVolume+"].");
 
 		openTheValve(index);
 		currentStartTime=millis();
@@ -108,7 +125,7 @@ void FeedTheLine(int index){
 			counter+=1;
 			delay(internalDelay); //ms
 			calculateCurrents();
-			checkTheFlow(counter, currentFlow);
+			//checkTheFlow(counter, currentFlow);
 		}
 	
 		stopThePump();
@@ -142,14 +159,22 @@ void calculateCurrents(){
 return;	
 }
 
+void startTheSensor() {
+	digitalWrite(pinConsumptionPower, HIGH);
+}
+
+void stopTheSensor() {
+	digitalWrite(pinConsumptionPower, LOW);
+}
+
 void startThePump() {
 	digitalWrite(pinPump, HIGH);
 }
-
 void stopThePump() {
 	digitalWrite(pinPump, LOW);
 }
 void openTheValve(int index) {
+	Serial.println((String) index+ "-valve opened. Pin-"+pinValves[index]);
 	digitalWrite(pinValves[index], HIGH);
 }
 
