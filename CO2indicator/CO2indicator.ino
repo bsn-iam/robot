189c4c3aed54454ab65d7b1db4b6d4d40bf49303
bsn-iam <bsn-iam@yandex.ru>
2018-05-12 23:38:39 +0300

    Workaround for MH-Z19b

--- CO2indicator/CO2indicator.ino
+++ CO2indicator/CO2indicator.ino
@@ -9,8 +9,31 @@ SSD1306AsciiWire oled;
 
 // CO2 sensor:
 SoftwareSerial SensorSerial(8,9); // RX,TX
-byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
-byte cmdRange[9] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x13,0x88,0xCB}; 
+byte cmdRead[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
+byte cmdRange5000[9] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x13,0x88,0xCB}; 
+
+byte cmdAbcOn[9] = {0xFF,0x01,0x79,0xA0,0x00,0x00,0x00,0x00,0xE6}; 
+byte cmdAbcOff[9] = {0xFF,0x01,0x79,0x00,0x00,0x00,0x00,0x00,0x86}; 
+byte cmdReset[9] = {0xFF,0x01,0x8d,0x00,0x00,0x00,0x00,0x00,0x72}; 
+
+// "\xFF\x01\x87\x00\x00\x00\x00\x00\x78"); ZERO POINT CALIBRATION
+// "\xFF\x01\x79\x00\x00\x00\x00\x00\x86"); ABC logic off
+// "\xFF\x01\x79\xA0\x00\x00\x00\x00\xE6"); ABC logic on
+//
+//calibrate = "\xFF\x01\x87\x00\x00\x00\x00\x00\x78"
+//abc_on    = "\xFF\x01\x79\xA0\x00\x00\x00\x00\xE6"
+//abc_off   = "\xFF\x01\x79\x00\x00\x00\x00\x00\x86"
+//read      = "\xFF\x01\x86\x00\x00\x00\x00\x00\x79"
+
+
+// mhzCmdCalibrateZero[]        = {0xFF,0x01,0x87,0x00,0x00,0x00,0x00,0x00,0x78};
+// mhzCmdABCEnable[]            = {0xFF,0x01,0x79,0xA0,0x00,0x00,0x00,0x00,0xE6};
+// mhzCmdABCDisable[]           = {0xFF,0x01,0x79,0x00,0x00,0x00,0x00,0x00,0x86};
+// mhzCmdReset[]                = {0xFF,0x01,0x8d,0x00,0x00,0x00,0x00,0x00,0x72};
+// mhzCmdMeasurementRange1000[] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x03,0xE8,0x7B};
+// mhzCmdMeasurementRange2000[] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x07,0xD0,0x8F};
+// mhzCmdMeasurementRange3000[] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x0B,0xB8,0xA3};
+// mhzCmdMeasurementRange5000[] = {0xFF,0x01,0x99,0x00,0x00,0x00,0x13,0x88,0xCB};
 unsigned char response[9];
 
 unsigned long th, tl, ppm = 0, ppm2 = 0, ppm3 = 0;
@@ -21,29 +44,30 @@ unsigned long th, tl, ppm = 0, ppm2 = 0, ppm3 = 0;
 const int pinDHT = 14;
 const int pinDHTpwm = 10;
 DHT dht(pinDHT, DHT22);
+const int stepTime = 10; //sec
+int time = 0;
 
 void setup() {
   // Serial
-  delay (5000);
+  delay (500);
   Serial.begin(9600);
   SensorSerial.begin(9600);
     
-  delay (5000);
+  delay (500);
   pinMode(pinDHTpwm, INPUT);
-  
-  SensorSerial.write(cmdRange, 9);
-  memset(response, 0, 9);
+
 
   // OLED
   Wire.begin();         
   oled.begin(&Adafruit128x32, I2C_ADDRESS);
   oled.set400kHz();  
   oled.setFont(ZevvPeep8x16);  
-  delay (1000);
+  delay (100);
   oled.clear();  
   oled.println("Initializing...");
   delay (5000);
   
+
   //DHT
 
   dht.begin();
@@ -53,58 +77,108 @@ long t = 0;
 
 void loop() 
 {
-  SensorSerial.write(cmd, 9);
-  memset(response, 0, 9);
-  
-  SensorSerial.readBytes(response, 9);
-  int i;
-  byte crc = 0;
-  for (i = 1; i < 8; i++) crc+=response[i];
-  crc = 255 - crc;
-  crc++;
+	//mh-z19b
+	String CRCmessage = GetCOdata();
 
-  
-  String CRCmessage = "";
-  
+	GetPpmPwm();
+
+	//DHT
+	String DHTmessage = GetDHTdata();
+	
+	oled.clear();  
+	oled.println(CRCmessage);
+	oled.println(DHTmessage);
+	
+	Serial.print(CRCmessage);
+	Serial.print(DHTmessage);
+	
+	delay(1000 * stepTime);
+	t += stepTime;
+	
+	//if (t == 30){
+	//	MHZ19Reset();
+	//	delay(1000 * stepTime);
+	//	t += stepTime;
+	//}
+}
+
+String GetCOdata(){
+	String message = "";
+	
+	SensorSerial.write(cmdRead, 9);
+	memset(response, 0, 9);
+	SensorSerial.readBytes(response, 9);
+	
+	//unsigned char r[9];
+	//r = response;
+	
+    Serial.println("Response is " +String(response[0])+"-"+String(response[1])+"-"+String(response[2])+"-"+String(response[3])+"-"+String(response[4])+"-"+String(response[5])+"-"+String(response[6])+"-"+String(response[7])+"-"+String(response[8]));
+
+
+	int i;
+	byte crc = 0;
+	for (i = 1; i < 8; i++) crc+=response[i];
+	crc = 255 - crc;
+	crc++;
+	
   if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
     Serial.println("CRC error: " + String(crc) + " / "+ String(response[8]));
-    //CRCmessage = "Sensor CRC error";
-    CRCmessage = "CRC heating...";
+    //message = "Sensor CRC error";
+    message = "No power...";
   } else {
     unsigned int responseHigh = (unsigned int) response[2];
     unsigned int responseLow = (unsigned int) response[3];
     unsigned int ppm = (256 * responseHigh) + responseLow;
-	ppm = 0.4 * ppm; // https://mysku.ru/blog/aliexpress/59397.html 
+	//ppm = 0.4 * ppm; // https://mysku.ru/blog/aliexpress/59397.html 
     //Serial.print(String(t)); Serial.print(","); Serial.print(ppm); Serial.println(";");
+	
+	unsigned long temp = response[4] - 40;
+	Serial.println("CO temp: " + String(temp) + " deg");
+	
     if (ppm <= 300 || ppm > 4900) {
-      CRCmessage = "CO2: " + String(ppm) + "(wrong)";
+      message = "CO2: " + String(ppm) + "(wrong)";
     } else {
-      CRCmessage = "CO2:" + String(ppm); 
+      message = "CO2:" + String(ppm); 
       if (ppm < 450) {   
-        CRCmessage += "(fine)";
+        message += "(fine)";
       }
       else if (ppm < 600) {   
-        CRCmessage += "(good)";
+        message += "(good)";
       }
       else if (ppm < 1000) {   
-        CRCmessage += "(accept)";
+        message += "(accept)";
       }
       else if (ppm < 2500) {   
-        CRCmessage += "(bad)";
+        message += "(bad)";
       }
 	  else {   
-        CRCmessage += "(alarm)";
+        message += "(alarm)";
       }
     }
   }
+	return message;
+}
 
-  
+
+String GetDHTdata(){
+	int h = dht.readHumidity();
+	int t = dht.readTemperature();
+	String message = "";
+	if (isnan(h) || isnan(t)) {
+		message = "Wrong DHT data";
+	} else {
+		message = "H: "+String(h)+"%  "+"T:"+String(t)+" C ";
+	}
+	return message;
+}
+
+void GetPpmPwm(){
     do {
     th = pulseIn(pinDHTpwm, HIGH, 1004000) / 1000;
     tl = 1004 - th;
     ppm2 =  2000 * (th-2)/(th+tl-4); // расчёт для диапазона от 0 до 2000ppm 
     ppm3 =  5000 * (th-2)/(th+tl-4); // расчёт для диапазона от 0 до 5000ppm 
-  } while (th == 0);
+	} while (th == 0);
 
   Serial.print(th);
   Serial.println(" <- Milliseconds PWM is HIGH");
@@ -112,35 +186,32 @@ void loop()
   Serial.println(" <- ppm2 (PWM) with 2000ppm as limit");
   Serial.print(ppm3);
   Serial.println(" <- ppm3 (PWM) with 5000ppm as limit");
-  
-  
-  
-  
-  
-  
-  
-  //DHT
-  int h = dht.readHumidity();
-  int t = dht.readTemperature();
-  
-  String DHTmessage = "";
-  
-  if (isnan(h) || isnan(t)) {
-	DHTmessage = "Wrong DHT data";
-  } else {
-    DHTmessage = "H: "+String(h)+"%  "+"T:"+String(t)+" C ";
-  }
-  
-  oled.clear();  
-  oled.println(CRCmessage);
-  oled.println(DHTmessage);
-  
-  Serial.print(CRCmessage);
-  Serial.print(DHTmessage);
-  
-
-  
-  
-  delay(10000);
-  t += 10;
 }
+
+
+void AbcOff(){
+	unsigned char r[9];
+	Serial.println("ABC off command... ");
+	SensorSerial.write(cmdAbcOff, 9);
+	memset(r, 0, 9);
+	Serial.println("Response is " +String(r[0])+"-"+String(r[1])+"-"+String(r[2])+"-"+String(r[3])+"-"+String(r[4])+"-"+String(r[5])+"-"+String(r[6])+"-"+String(r[7])+"-"+String(r[8]));
+}
+
+void MHZ19Reset(){
+	unsigned char r[9];
+	Serial.println("MHZ19Reset command... ");
+	SensorSerial.write(cmdReset, 9);
+	memset(r, 0, 9);
+	Serial.println("Response is " +String(r[0])+"-"+String(r[1])+"-"+String(r[2])+"-"+String(r[3])+"-"+String(r[4])+"-"+String(r[5])+"-"+String(r[6])+"-"+String(r[7])+"-"+String(r[8]));
+}
+
+void AbcOn(){
+	unsigned char r[9];
+	Serial.println("ABC on command... ");
+	SensorSerial.write(cmdAbcOn, 9);
+	memset(r, 0, 9);
+	Serial.println("Response is " +String(r[0])+"-"+String(r[1])+"-"+String(r[2])+"-"+String(r[3])+"-"+String(r[4])+"-"+String(r[5])+"-"+String(r[6])+"-"+String(r[7])+"-"+String(r[8]));
+}
+
+
+
